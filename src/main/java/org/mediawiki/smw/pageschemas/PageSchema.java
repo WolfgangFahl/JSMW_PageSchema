0135609103d4c356ac1c52155f680bd05e5a30f4
Wolfgang Fahl <wf@bitplan.com>
2014-10-13 16:05:31 +0200

    adds Java Wiki api and test

--- src/main/java/org/mediawiki/smw/pageschemas/PageSchema.java
+++ src/main/java/org/mediawiki/smw/pageschemas/PageSchema.java
@@ -11,6 +11,7 @@ import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
 
 
 /**
@@ -19,6 +20,7 @@ import javax.xml.bind.annotation.XmlRootElement;
  * @author wf
  */
 @XmlRootElement(name="PageSchema")
+@XmlType(propOrder = { "forms", "templates", "sections" })
 public class PageSchema {
 	List<Template> templates=new ArrayList<Template>();
 	List<Form> forms=new ArrayList<Form>();
---
+++ src/test/java/org/mediawiki/smw/pageschemas/TestWiki.java
@@ -0,0 +1,94 @@
+package org.mediawiki.smw.pageschemas;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Base64;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.security.auth.login.FailedLoginException;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.wikipedia.Wiki;
+import org.wikipedia.XTrustProvider;
+
+/**
+ * test Wiki interface
+ * 
+ * @author wf
+ *
+ */
+public class TestWiki {
+	protected static Logger LOGGER = Logger
+			.getLogger("org.mediawiki.smw.pageschemas");
+
+	static Properties props;
+	private static String propsPath;
+
+	/**
+	 * decode the given string
+	 * @param str
+	 * @return
+	 */
+	public String decode(String str) {
+	  //Decode data on other side, by processing encoded data
+		String result=new String(Base64.getDecoder().decode(str));
+		return result;
+	}
+
+	/**
+	 * encode the given string
+	 * @param str
+	 * @return
+	 */
+	public String encode(String str) {
+	  //encode data on your side using BASE64
+		String result=Base64.getEncoder().encodeToString(str.getBytes());
+		return result;
+	}
+
+	@BeforeClass
+	public static void readProps() {
+		propsPath = System.getProperty("user.home") + "/.testWiki/properties.txt";
+		File propsFile = new File(propsPath);
+		if (propsFile.canRead()) {
+			props = new Properties();
+			try {
+				props.load(new FileInputStream(propsFile));
+			} catch (FileNotFoundException e) {
+			} catch (IOException e) {
+			}
+		}
+	}
+
+	@Test
+	public void testLogin() throws FailedLoginException, IOException {
+		if (props != null) {
+			String prot=props.getProperty("prot");
+			String domain = props.getProperty("domain");
+			String scriptpath = props.getProperty("scriptpath");
+			String username = props.getProperty("username");
+			String password = props.getProperty("password");
+			password =decode(password.trim()).replace("\r", "").replace("\n", "");
+			LOGGER.log(Level.INFO, domain + " " + username + " '" + password+"'");
+
+			// http://stackoverflow.com/questions/7615645/ssl-handshake-alert-unrecognized-name-error-since-upgrade-to-java-1-7-0
+			System.setProperty("jsse.enableSNIExtension", "false");
+			// http://stackoverflow.com/questions/3093112/certificateexception-no-name-matching-ssl-someurl-de-found
+			HttpsURLConnection
+					.setDefaultHostnameVerifier((hostname, session) -> true);
+			// https://code.google.com/p/misc-utils/wiki/JavaHttpsUrl
+			XTrustProvider.install();
+			Wiki wiki = new Wiki(prot,domain, scriptpath);
+			wiki.login(username, password);
+		} else {
+			System.err.println("test not run due to missing file " + propsPath);
+		}
+	}
+
+}
